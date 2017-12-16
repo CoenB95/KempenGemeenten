24068f2a0ccdad1839c32ea47b465409799e1ea8
CoenB95 <boelhouwers.coen@gmail.com>
2017-12-16 20:12:51 +0100

    Start experimenting with DrawerLayout.

--- app/src/main/java/com/cbapps/kempengemeenten/MainActivity.java
+++ app/src/main/java/com/cbapps/kempengemeenten/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_screen);
+		setContentView(R.layout.main_layout);
 
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
