63b8035b0ed293bd99f8dde1012de3c64cbf1cfd
CoenB95 <boelhouwers.coen@gmail.com>
2018-01-02 13:42:03 +0100

    Directions.

--- app/src/main/java/com/cbapps/kempengemeenten/nextgen/fragments/MapFragment.java
+++ app/src/main/java/com/cbapps/kempengemeenten/nextgen/fragments/MapFragment.java
@@ -2,11 +2,23 @@ package com.cbapps.kempengemeenten.nextgen.fragments;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.cb.kempengemeenten.R;
 import com.cbapps.kempengemeenten.nextgen.CoordinateConverter;
 import com.cbapps.kempengemeenten.nextgen.database.LmsDatabase;
 import com.cbapps.kempengemeenten.nextgen.database.LmsPoint;
@@ -18,6 +30,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -34,8 +50,11 @@ import java.util.concurrent.Executors;
 
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
 
+	public static final String TAG = "MapFragment";
+
 	private ExecutorService service;
 	private Handler handler;
+	private GoogleMap googleMap;
 
 	public MapFragment() {
 		service = Executors.newCachedThreadPool();
@@ -47,6 +66,7 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 	@SuppressLint("MissingPermission")
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
+		this.googleMap = googleMap;
 		Toast.makeText(getContext(), "Map initialized", Toast.LENGTH_SHORT).show();
 		PermissionManager.requestPermission(getActivity(), PermissionManager.READ_PERMISSION_REQUEST_CODE, (requestCode, results) -> {
 			for (PermissionManager.PermissionResult result : results) {
@@ -66,6 +86,58 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 		}, Manifest.permission.ACCESS_FINE_LOCATION);
 	}
 
+	public void onPointSelected(LmsPoint point) {
+		if (getContext() == null) {
+			Log.d(TAG, "onPointSelected: no context");
+			return;
+		}
+
+		if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+				!= PackageManager.PERMISSION_GRANTED) {
+			Log.d(TAG, "onPointSelected: no permission");
+			return;
+		}
+
+		LocationManager manager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+		if (manager == null) {
+			Log.d(TAG, "onPointSelected: no manager");
+			return;
+		}
+
+		Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if (location == null) {
+			Log.d(TAG, "onPointSelected: no known location");
+			return;
+		}
+
+		CoordinateConverter cc = new RDToWGS84Converter();
+		LatLng latLng = cc.toLatLng(point.rdX, point.rdY);
+		String apiKey = getString(R.string.google_maps_key);
+		RequestQueue queue = Volley.newRequestQueue(getContext());
+		queue.add(new JsonObjectRequest(Request.Method.GET,
+				"https://maps.googleapis.com/maps/api/directions/json?" +
+						"origin=" + location.getLatitude() + "," + location.getLongitude() +
+						"&destination=" + latLng.latitude + "," + latLng.longitude +
+						"&mode=driving" +
+						"&key=" + apiKey, null, response -> {
+			Log.w(TAG, "We got a response!");
+			try {
+				String encodedPoly = response.getJSONArray("routes")
+						.getJSONObject(0)
+						.getJSONObject("overview_polyline")
+						.getString("points");
+
+				googleMap.addPolyline(new PolylineOptions()
+						.addAll(PolyUtil.decode(encodedPoly))
+						.color(ContextCompat.getColor(getContext(), R.color.colorAccent)));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}, error -> {
+			Log.w(TAG, "Too bad!");
+		}));
+	}
+
 	private void loadCSV(GoogleMap googleMap) {
 		service.submit(() -> {
 			List<LmsPoint> points = LmsDatabase.newInstance(getContext()).lmsDao().getAll();
@@ -80,6 +152,9 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 							.snippet(point.address));
 					marker.setTag(point.getLmsNumber());
 				}
+
+				if (points.size() > 0)
+					onPointSelected(points.get(0));
 			});
 		});
 	}
